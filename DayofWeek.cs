e9b9b8c3bd2b40dfd802d3424fa2e173a4d44d66
vpradhap <pradhapvenkat@gmail.com>
2022-05-11 21:58:04 +0530

    Added : Finding Day of The Week Program

---
+++ DayofWeek.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit_Testing
+{
+    public class DayofWeek
+    {
+        public static void DayWeek()
+        {
+            //User input 
+            Console.Write("\nEnter a Month : ");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a Day : ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a Year : ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            //Gregorian calendar formulas
+            int y0 = year - (14 - month) / 12;
+            int x = y0 + (y0 / 4) - (y0 / 100) + (y0 / 400);
+            int m0 = month + 12 * ((14 - month) / 12) - 2;
+            int d0 = (day + x + 31 * m0 / 12) % 7;
+
+            string[] days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            Console.WriteLine("\nDay Of The Week : " + days[d0]);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿
-Console.WriteLine("\tNUnit Testing Programs\n");
+﻿using NUnit_Testing;
+
+Console.WriteLine("\n\tN-Unit Testing Programs\n");
+Console.WriteLine("0 - Enter Zero to Exit ");
+Console.WriteLine("1 - Day of Week Program");
+
+int flag = 0;
+while (flag == 0)
+{
+    Console.Write("\nPick A Program To Perform : ");
+    int pick = Convert.ToInt32(Console.ReadLine());
+    switch (pick)
+    {
+        case 0:
+            flag = 1;
+            break;
+        case 1:
+            DayofWeek.DayWeek();
+            break;
+        default:
+            Console.WriteLine("Invalid Pick");
+            break;
+    }
+}
